3fe3baafbd5b6995938fb972ff5dcc12b9334b41
zSm0k3 <thexof02@gmail.com>
2024-02-22 10:43:46 -0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Quévin Willian da Silva</Text>
+      <Text>Desenvolvimento de Dispositivos Móveis - 22/02/2024</Text>
       <StatusBar style="auto" />
     </View>
   );
